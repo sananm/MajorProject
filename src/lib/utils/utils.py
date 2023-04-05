4be50c8fe976228c79c1df82722cddb9531cce37
sananm <mohammedsanan18@gmail.com>
2023-04-05 19:28:23 +0530

    test build v1

---
+++ src/lib/utils/utils.py
@@ -0,0 +1,331 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import numpy as np
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        if self.count > 0:
+          self.avg = self.sum / self.count
+
+
+def xyxy2xywh(x):
+    # Convert bounding box format from [x1, y1, x2, y2] to [x, y, w, h]
+    y = torch.zeros(x.shape) if x.dtype is torch.float32 else np.zeros(x.shape)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2
+    y[:, 2] = x[:, 2] - x[:, 0]
+    y[:, 3] = x[:, 3] - x[:, 1]
+    return y
+
+
+def xywh2xyxy(x):
+    # Convert bounding box format from [x, y, w, h] to [x1, y1, x2, y2]
+    y = torch.zeros(x.shape) if x.dtype is torch.float32 else np.zeros(x.shape)
+    y[:, 0] = (x[:, 0] - x[:, 2] / 2)
+    y[:, 1] = (x[:, 1] - x[:, 3] / 2)
+    y[:, 2] = (x[:, 0] + x[:, 2] / 2)
+    y[:, 3] = (x[:, 1] + x[:, 3] / 2)
+    return y
+
+def ap_per_class(tp, conf, pred_cls, target_cls):
+    """ Compute the average precision, given the recall and precision curves.
+    Method originally from https://github.com/rafaelpadilla/Object-Detection-Metrics.
+    # Arguments
+        tp:    True positives (list).
+        conf:  Objectness value from 0-1 (list).
+        pred_cls: Predicted object classes (list).
+        target_cls: True object classes (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+
+    # lists/pytorch to numpy
+    tp, conf, pred_cls, target_cls = np.array(tp), np.array(conf), np.array(pred_cls), np.array(target_cls)
+
+    # Sort by objectness
+    i = np.argsort(-conf)
+    tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
+
+    # Find unique classes
+    unique_classes = np.unique(np.concatenate((pred_cls, target_cls), 0))
+
+    # Create Precision-Recall curve and compute AP for each class
+    ap, p, r = [], [], []
+    for c in unique_classes:
+        i = pred_cls == c
+        n_gt = sum(target_cls == c)  # Number of ground truth objects
+        n_p = sum(i)  # Number of predicted objects
+
+        if (n_p == 0) and (n_gt == 0):
+            continue
+        elif (n_p == 0) or (n_gt == 0):
+            ap.append(0)
+            r.append(0)
+            p.append(0)
+        else:
+            # Accumulate FPs and TPs
+            fpc = np.cumsum(1 - tp[i])
+            tpc = np.cumsum(tp[i])
+
+            # Recall
+            recall_curve = tpc / (n_gt + 1e-16)
+            r.append(tpc[-1] / (n_gt + 1e-16))
+
+            # Precision
+            precision_curve = tpc / (tpc + fpc)
+            p.append(tpc[-1] / (tpc[-1] + fpc[-1]))
+
+            # AP from recall-precision curve
+            ap.append(compute_ap(recall_curve, precision_curve))
+
+    return np.array(ap), unique_classes.astype('int32'), np.array(r), np.array(p)
+
+
+def compute_ap(recall, precision):
+    """ Compute the average precision, given the recall and precision curves.
+    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+    # Arguments
+        recall:    The recall curve (list).
+        precision: The precision curve (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+    # correct AP calculation
+    # first append sentinel values at the end
+
+    mrec = np.concatenate(([0.], recall, [1.]))
+    mpre = np.concatenate(([0.], precision, [0.]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
+def bbox_iou(box1, box2, x1y1x2y2=False):
+    """
+    Returns the IoU of two bounding boxes
+    """
+    N, M = len(box1), len(box2)
+    if x1y1x2y2:
+        # Get the coordinates of bounding boxes
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+    else:
+        # Transform from center and width to exact coordinates
+        b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+        b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+        b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+        b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+
+    # get the coordinates of the intersection rectangle
+    inter_rect_x1 = torch.max(b1_x1.unsqueeze(1), b2_x1)
+    inter_rect_y1 = torch.max(b1_y1.unsqueeze(1), b2_y1)
+    inter_rect_x2 = torch.min(b1_x2.unsqueeze(1), b2_x2)
+    inter_rect_y2 = torch.min(b1_y2.unsqueeze(1), b2_y2)
+    # Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1, 0) * torch.clamp(inter_rect_y2 - inter_rect_y1, 0)
+    # Union Area
+    b1_area = ((b1_x2 - b1_x1) * (b1_y2 - b1_y1))
+    b1_area = ((b1_x2 - b1_x1) * (b1_y2 - b1_y1)).view(-1,1).expand(N,M)
+    b2_area = ((b2_x2 - b2_x1) * (b2_y2 - b2_y1)).view(1,-1).expand(N,M)
+
+    return inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+
+def generate_anchors(nGh, nGw, anchor_wh):
+    nA = len(anchor_wh)
+    yy, xx = np.meshgrid(np.arange(nGh), np.arange(nGw), indexing='ij')
+
+    mesh = np.stack([xx, yy], axis=0)  # Shape 2, nGh, nGw
+    mesh = np.tile(np.expand_dims(mesh, axis=0), (nA, 1, 1, 1)) # Shape nA x 2 x nGh x nGw
+    anchor_offset_mesh = np.tile(np.expand_dims(np.expand_dims(anchor_wh, -1), -1), (1, 1, nGh, nGw))  # Shape nA x 2 x nGh x nGw
+    anchor_mesh = np.concatenate((mesh, anchor_offset_mesh), axis=1)  # Shape nA x 4 x nGh x nGw
+    return anchor_mesh
+
+
+def encode_delta(gt_box_list, fg_anchor_list):
+    px, py, pw, ph = fg_anchor_list[:, 0], fg_anchor_list[:,1], \
+                     fg_anchor_list[:, 2], fg_anchor_list[:,3]
+    gx, gy, gw, gh = gt_box_list[:, 0], gt_box_list[:, 1], \
+                     gt_box_list[:, 2], gt_box_list[:, 3]
+    dx = (gx - px) / pw
+    dy = (gy - py) / ph
+    dw = np.log(gw/pw)
+    dh = np.log(gh/ph)
+    return np.stack((dx, dy, dw, dh), axis=1)
+
+
+
+
+#######################  DEPTH ESTIMATE UTILS  #######################
+
+
+import numpy as np
+from PIL import Image
+
+def DepthNorm(x, maxDepth):
+    return maxDepth / x
+
+def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
+    # Support multiple RGBs, one RGB image, even grayscale 
+    if len(images.shape) < 3: images = np.stack((images,images,images), axis=2)
+    if len(images.shape) < 4: images = images.reshape((1, images.shape[0], images.shape[1], images.shape[2]))
+    # Compute predictions
+    predictions = model.predict(images, batch_size=batch_size)
+    # Put in expected range
+    return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
+
+def scale_up(scale, images):
+    from skimage.transform import resize
+    scaled = []
+    
+    for i in range(len(images)):
+        img = images[i]
+        output_shape = (scale * img.shape[0], scale * img.shape[1])
+        scaled.append( resize(img, output_shape, order=1, preserve_range=True, mode='reflect', anti_aliasing=True ) )
+
+    return np.stack(scaled)
+
+def load_images(image_files):
+    loaded_images = []
+    for file in image_files:
+        x = np.clip(np.asarray(Image.open( file ), dtype=float) / 255, 0, 1)
+        loaded_images.append(x)
+    
+    return np.stack(loaded_images, axis=0)
+
+def to_multichannel(i):
+    if i.shape[2] == 3: return i
+    i = i[:,:,0]
+    return np.stack((i,i,i), axis=2)
+        
+def display_images(outputs, inputs=None, gt=None, is_colormap=True, is_rescale=True):
+    import matplotlib.pyplot as plt
+    import skimage
+    from skimage.transform import resize
+
+    plasma = plt.get_cmap('plasma')
+
+    shape = (outputs[0].shape[0], outputs[0].shape[1], 3)
+    
+    all_images = []
+
+    for i in range(outputs.shape[0]):
+        imgs = []
+        
+        if isinstance(inputs, (list, tuple, np.ndarray)):
+            x = to_multichannel(inputs[i])
+            x = resize(x, shape, preserve_range=True, mode='reflect', anti_aliasing=True )
+            imgs.append(x)
+
+        if isinstance(gt, (list, tuple, np.ndarray)):
+            x = to_multichannel(gt[i])
+            x = resize(x, shape, preserve_range=True, mode='reflect', anti_aliasing=True )
+            imgs.append(x)
+
+        if is_colormap:
+            rescaled = outputs[i][:,:,0]
+            if is_rescale:
+                rescaled = rescaled - np.min(rescaled)
+                rescaled = rescaled / np.max(rescaled)
+            imgs.append(plasma(rescaled)[:,:,:3])
+        else:
+            imgs.append(to_multichannel(outputs[i]))
+
+        img_set = np.hstack(imgs)
+        all_images.append(img_set)
+
+    all_images = np.stack(all_images)
+    
+    return skimage.util.montage(all_images, multichannel=True, fill=(0,0,0))
+
+def save_images(filename, outputs, inputs=None, gt=None, is_colormap=True, is_rescale=False):
+    montage =  display_images(outputs, inputs, is_colormap, is_rescale)
+    im = Image.fromarray(np.uint8(montage*255))
+    im.save(filename)
+
+def load_test_data(test_data_zip_file='nyu_test.zip'):
+    print('Loading test data...', end='')
+    import numpy as np
+    from data import extract_zip
+    data = extract_zip(test_data_zip_file)
+    from io import BytesIO
+    rgb = np.load(BytesIO(data['eigen_test_rgb.npy']))
+    depth = np.load(BytesIO(data['eigen_test_depth.npy']))
+    crop = np.load(BytesIO(data['eigen_test_crop.npy']))
+    print('Test data loaded.\n')
+    return {'rgb':rgb, 'depth':depth, 'crop':crop}
+
+def compute_errors(gt, pred):
+    thresh = np.maximum((gt / pred), (pred / gt))
+    a1 = (thresh < 1.25   ).mean()
+    a2 = (thresh < 1.25 ** 2).mean()
+    a3 = (thresh < 1.25 ** 3).mean()
+    abs_rel = np.mean(np.abs(gt - pred) / gt)
+    rmse = (gt - pred) ** 2
+    rmse = np.sqrt(rmse.mean())
+    log_10 = (np.abs(np.log10(gt)-np.log10(pred))).mean()
+    return a1, a2, a3, abs_rel, rmse, log_10
+
+def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False):
+    N = len(rgb)
+
+    bs = batch_size
+
+    predictions = []
+    testSetDepths = []
+    
+    for i in range(N//bs):    
+        x = rgb[(i)*bs:(i+1)*bs,:,:,:]
+        
+        # Compute results
+        true_y = depth[(i)*bs:(i+1)*bs,:,:]
+        pred_y = scale_up(2, predict(model, x/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
+        
+        # Test time augmentation: mirror image estimate
+        pred_y_flip = scale_up(2, predict(model, x[...,::-1,:]/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
+
+        # Crop based on Eigen et al. crop
+        true_y = true_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
+        pred_y = pred_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
+        pred_y_flip = pred_y_flip[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
+        
+        # Compute errors per image in batch
+        for j in range(len(true_y)):
+            predictions.append(   (0.5 * pred_y[j]) + (0.5 * np.fliplr(pred_y_flip[j]))   )
+            testSetDepths.append(   true_y[j]   )
+
+    predictions = np.stack(predictions, axis=0)
+    testSetDepths = np.stack(testSetDepths, axis=0)
+
+    e = compute_errors(predictions, testSetDepths)
+
+    if verbose:
+        print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format('a1', 'a2', 'a3', 'rel', 'rms', 'log_10'))
+        print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(e[0],e[1],e[2],e[3],e[4],e[5]))
+
+    return e
